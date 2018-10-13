6f83e3570cb8e2d9cc7660329c3193976bd7837c
leonardo15043 <lahernandez15043@gmail.com>
2018-10-13 16:24:32 -0500

    Initial Commit

---
+++ qr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qr
+//
+//  Created by leonardo hernandez on 10/13/18.
+//  Copyright © 2018 leonardo hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
